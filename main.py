8f17cc6367cb5e6db906d8bb12f1de5da405e30c
Maksym Solomyanov <maksym.solomyanov@gmail.com>
2024-08-13 11:37:54 +0200

    Add database interaction widgets.

--- main.py
+++ main.py
@@ -49,4 +49,21 @@ l_email.place(x=10, y=110)
 e_email = Entry(frame_down, width=25, justify='left', highlightthickness=1, relief="solid")
 e_email.place(x=80, y=110)
 
+b_search = Button(frame_down, text="Search", height=1, bg=co2, fg=co0, font=('Ivy 8 bold'))
+b_search.place(x=290, y=20)
+e_search = Entry(frame_down, width=16, justify='left', font=('Ivy, 11'), highlightthickness=1, relief="solid")
+e_search.place(x=347, y=20)
+
+b_view = Button(frame_down, text="View", width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'))
+b_view.place(x=290, y=50)
+
+b_add = Button(frame_down, text="Add", width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'))
+b_add.place(x=400, y=50)
+
+b_update = Button(frame_down, text="Update", width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'))
+b_update.place(x=400, y=80)
+
+b_delete = Button(frame_down, text="Delete", width=10, height=1, bg=co2, fg=co0, font=('Ivy 8 bold'))
+b_delete.place(x=400, y=110)
+
 window.mainloop()
